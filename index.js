3fa49ae7c635461252712e0fac9eada55096d862
tesfu <tesfu9503@gmail.com>
2022-04-26 20:47:21 +0300

    make it better

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 require('dotenv').config()
 const app = express();
 const authRouter = require('./routes/auth')
-const userRouter = require('./routes/user')
+// const userRouter = require('./routes/user')
 const ProductRouter = require('./routes/product')
 const CartRouter = require('./routes/cart')
 const OrderRouter = require('./routes/Order')
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URL)
 const PORT = process.env.PORT || 5000;
 
 app.use('/api',authRouter)
-app.use('/api/user',userRouter)
+// app.use('/api/user',userRouter)
 app.use('/api/product',ProductRouter)
 app.use('/api/cart',CartRouter)
 app.use('/api/order',OrderRouter)
